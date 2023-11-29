4ac9010750ba1767c8d7cdaec9350dd585cc64ef
mohran <mohamed01098187312@gmail.com>
2023-11-29 16:32:30 +0200

    the crud of spring jdbc

---
+++ src/main/java/com/pluralsight/repository/RideRepository.java
@@ -0,0 +1,21 @@
+package com.pluralsight.repository;
+
+import java.util.List;
+
+import com.pluralsight.model.Ride;
+
+public interface RideRepository {
+
+	List<Ride> getRides();
+
+	Ride createRide(Ride ride);
+
+	Ride getRide(Integer id);
+
+	Ride updateRide(Ride ride);
+
+	void updateRide(List<Object[]> pairs);
+
+	void deleteRide(Integer id);
+
+}
---
+++ src/main/java/com/pluralsight/service/RideService.java
@@ -0,0 +1,24 @@
+package com.pluralsight.service;
+
+import java.util.List;
+
+import com.pluralsight.model.Ride;
+
+public interface RideService {
+
+	
+	List<Ride> getRides();
+
+	Ride createRide(Ride ride);
+
+	Ride getRide(Integer id);
+
+	Ride updateRide(Ride ride);
+  
+	void batch();
+
+	void deleteRide(Integer id);
+
+
+
+}
